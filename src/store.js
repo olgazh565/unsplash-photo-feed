9e4a957e0bd58da8b6154f2615cdd5cadcc8d1ed
olgazh565 <olgazh565@gmail.com>
2024-01-19 18:36:10 +0300

    imports fixed

--- src/store.js
+++ src/store.js
@@ -1,10 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
-import tokenReducer, {tokenMiddleware} from './assets/store/tokenSlice';
 import authReducer from './assets/store/authSlice';
 import fotosReducer from './assets/store/fotosSlice';
 import singleFotoReducer from './assets/store/singleFotoSlice';
 import likeReducer from './assets/store/likeSlice';
 import userLikesReducer from './assets/store/userLikesSlice';
+import tokenReducer, {tokenMiddleware} from './assets/store/tokenSlice';
 
 export const store = configureStore({
   reducer: {
