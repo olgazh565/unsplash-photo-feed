c14af68ca9b5efc705623ecc0f783552b7d91765
olgazh565 <olgazh565@gmail.com>
2024-01-19 19:14:20 +0300

    import fixed

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
-import App from './App.jsx';
+import App from './App';
 import {Provider} from 'react-redux';
-import {store} from './store.js';
+import {store} from './store';
 import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
